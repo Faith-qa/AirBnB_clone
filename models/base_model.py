1e69efd49007e27dead36c9bdbcd655584872b2a
Faith-qa <atienofaith12@gmail.com>
2021-03-01 11:10:44 +0300

    corrected errors

--- models/base_model.py
+++ models/base_model.py
@@ -45,7 +45,7 @@ you can use isoformat() of datetime object
         TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
         self.id = str(uuid4())
-        self.created_at = datetime.()
+        self.created_at = datetime.now()
         self.updated_at = self.created_at
 
         if len(kwargs) != 0:
